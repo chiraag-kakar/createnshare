87c6fa1eca2b8861d41a0750ff44ac4442ddf695
chiraag-kakar <chirucos1998@gmail.com>
2022-02-01 18:31:10 +0530

    hard-coded db path

--- createnshare/settings.py
+++ createnshare/settings.py
@@ -111,13 +111,10 @@ WSGI_APPLICATION = 'createnshare.wsgi.application'
 #             'PORT': os.getenv('DB_PORT')
 #         }
 #     }
+
 # Production DB Configuration
-if os.getenv('GITHUB_WORKFLOW'):
-    DATABASES = {}
-    DATABASES['default'] = env('DATABASE_URL').config(default='DATABASE_URL')
-else:
-    DATABASES = {}
-    DATABASES['default'] = env('DATABASE_URL').config(default='DATABASE_URL')
+DATABASES = {}
+DATABASES['default'] = env('postgres://nmgosibpspofxx:bf3caa8c2b1a2db913ce11a116ca60d46bccbe5d7ee871c6178e505a03079187@ec2-34-205-46-149.compute-1.amazonaws.com:5432/d4429ul1k862js').config(default='DATABASE_URL')
 
 
 
